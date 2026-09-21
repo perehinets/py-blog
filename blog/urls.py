fa3eb211bdc7ca60d8a9dff53286273075ed4c13
Vasyl Perehinets <perehinets95@gmail.com>
2026-09-21 22:28:37 +0100

    blog

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from .views import index, PostDetailView, CommentaryCreateView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("post/<int:pk>", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>", PostDetailView.as_view(), name="post-detail"),
     path(
-        "post/<int:pk>/comment/",
+        "posts/<int:pk>/comment/",
         CommentaryCreateView.as_view(),
         name="comment-create"
     ),
